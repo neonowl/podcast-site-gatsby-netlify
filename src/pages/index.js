8283b734d9f65fe47cc5dacb534557c9ca266730
jerm <jerm@neonowl.net>
2020-01-21 09:59:37 -0500

    new component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 import { Link } from "gatsby"
 
 export default () => (
-  <>
+  <Layout>
     <h1>Podcast Site</h1>
     <h2>With a podcast feed</h2>
     <p>Subscribe via <Link to="/rss.xml">rss feed</Link>.</p>
-  </>
+  </Layout>
 )
